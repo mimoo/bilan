27b125404ca8d070dd2aae67cf7391ba2f16b34f
David Wong <davidwong.crypto@gmail.com>
2022-07-30 01:07:37 +0200

    fix github baseUrl

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+const config = {
   plugins: [vue({      reactivityTransform: true  })],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
+};
+
+export default defineConfig(({ command, mode }) => {
+  if (command === 'serve') {
+    return config;
+  } else {
+    return { base: '/bilan/', ...config };
+  }
 })
